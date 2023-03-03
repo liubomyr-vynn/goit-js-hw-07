74fa6da4b9363373f99eda04b5240711e7e3ff0c
liubomyr-vynn <pro.vynn.l@gmail.com>
2023-03-03 12:27:06 +0200

    02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+	.map(
+		galleryItem =>
+			`<div class="gallery__item">
+        <a class="gallery__item" href="${galleryItem.original}">
+          <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+        </a>
+      </div>`,
+	)
+	.join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+	animationSpeed: 300,
+});
